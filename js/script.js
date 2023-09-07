2e688e4e9c993ecacdb0e859029a7928d5a68280
Galyna <minimal005@gmail.com>
2023-09-07 17:02:32 +0300

    added 11-14

--- js/script.js
+++ js/script.js
@@ -145,13 +145,15 @@ const nowTime = () => {
 
     function getTime () {
         newClock = new Date()
-        hour.textContent = `${newClock.getHours()} : `
-        minute.textContent = `${newClock.getMinutes()} : `
+        hour.textContent = newClock.getHours()
+        minute.textContent = newClock.getMinutes()
         second.textContent = newClock.getSeconds()
 
         for (let elem of time){
             correctTime(elem) 
          }
+         hour.textContent += ' : '
+         minute.textContent += ' : '
     }
 // корректне відображення часу
     function correctTime(arg){
@@ -180,7 +182,6 @@ class Scedule {
         this.beginTime = beginTime
         this.duringTime = duringTime
         this.allScedule = [,[],[],[],[],[],[],[]]
-        this.week = ['понеділок', 'вівторок', 'середа', 'четвер', 'пятниця', 'субота', 'неділя']
     }
     // • дані по заняттю задаємо масивом [назва предмета, день тижня (числом), початковий час заняття, тривалість заняття]
     addLesson(lesson){
@@ -246,4 +247,249 @@ class Scedule {
 // • Додавання нового автобуса до компанії
 // • Видалення автобуса з компанії
 // • Перегляд списку всіх автобусів компанії
-// • Перегляд списку автобусів, які доступні для бронювання на певний день та час
+// • Перегляд списку автобусів, які доступні для бронювання на певний день
+
+class BusCompany {
+    constructor(nameCompany){
+        this.nameCompany = nameCompany
+        this.allBuses = []
+        this.bookingBus = {}
+    }
+    addBus(nameBus){
+        this.allBuses.push(nameBus)
+        this.bookingBus[nameBus] = []
+    }
+    removeBus(nameBus){
+        this.allBuses = this.allBuses.filter(elem => elem != nameBus)
+        if (nameBus in this.bookingBus) {
+            delete this.bookingBus[nameBus]}
+        return this.bookingBus
+    }
+    // • Перегляд списку всіх автобусів компанії
+    showListAllBuses(){
+        return this.allBuses.join(' | ')
+    }
+    // коректне представлення дати у вигляді рядку
+    correctDate(date){
+        const month = [,'january','february', 'match', 'april', 'may', 'june', 'july', 'august', 'september', 'november', 'october', 'december']
+        return `${date[0]} ${month[date[1]]} ${date[2]}`
+
+    }
+    // • замовлення автобуса на певну дату. Дату вводимо масивом [число, місяць, рік]
+    bookingBusOnDate(nameBus, date){
+        for(let elem of this.bookingBus[nameBus]){
+            if (elem == this.correctDate(date)) return console.log('На цю годину автобус замовлено')
+        }
+        return this.bookingBus[nameBus].push(this.correctDate(date))
+    }
+    // • Перегляд списку автобусів, які доступні для бронювання на певний день. Дату вводимо масивом [число, місяць, рік]
+    showAllBusWithFreeTime(date){
+        let freeBus = []
+        console.log(this.bookingBus);
+        for(let bus in this.bookingBus){
+          if(!this.bookingBus[bus].includes(this.correctDate(date))) freeBus.push(bus)
+        }
+        return freeBus
+    }
+}
+
+const proto = new BusCompany('Proto')
+// proto.addBus('Icarus')
+// proto.addBus('Icarus2')
+// proto.addBus('Icarus3')
+// proto.addBus('Icarus4')
+// proto.removeBus('Icarus')
+// console.log(proto.showListAllBuses())
+// proto.bookingBusOnDate('Icarus2', [8, 9, 2023])// замовимо на 8.9.2023
+// proto.bookingBusOnDate('Icarus2', [8, 9, 2023])// замовимо на 8.9.2023 / 'На цю годину автобус замовлено'
+// proto.bookingBusOnDate('Icarus2', [6, 9, 2023])// замовимо на 6.9.2023
+// proto.bookingBusOnDate('Icarus2', [7, 9, 2023])
+// console.log(proto.showAllBusWithFreeTime([6, 9, 2023])); // ['Icarus3', 'Icarus4'], оскільки перший видалено, а в другого на цю дату є замовлення
+
+
+// 11. Створіть клас Person з наступними властивостями:
+// • name - рядок, що містить ім'я особи;
+// • age - число, що містить вік особи;
+// • gender - рядок, що містить стать особи.
+// • Створіть метод greet(), який повинен повернути рядок з привітанням ім'ям людини, якщо її вік більше 18 років, або повідомлення "Ви ще надто молоді" в іншому випадку.
+
+// • Створіть клас Student, який наслідується від класу Person. 
+// Додайте до класу наступні властивості:
+// o course - число, що містить номер курсу, на якому 
+// навчається студент;
+// o university - рядок, що містить назву університету, в якому 
+// навчається студент.
+// • Створіть метод study(), який повинен повідомляти про те, що 
+// студент навчається на певному курсі в певному університеті.
+
+// • Створіть клас Teacher, який наслідується від класу Person. 
+// Додайте до класу наступні властивості:
+// o subject - рядок, що містить назву предмету, який викладає 
+// викладач;
+// o university - рядок, що містить назву університету, в якому 
+// викладає викладач.
+// • Створіть метод teach(), який повинен повідомляти про те, що 
+// викладач викладає певний предмет в певному університеті.
+// • Створіть об'єкти класів Student і Teacher, заповніть їх 
+// властивості і викличте методи study() та teach(). Перевірте, чи 
+// працюють всі методи і властивості класів.
+
+class Person {
+    constructor(name, age, gender){
+        this.name = name
+        this.age = age
+        this.gender = gender
+    }
+    greet(){
+       return this.age > 18 ? `Вітаємо, ${this.name}` : `${this.name}, Ви ще надто молоді`
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, gender, course, university){
+        super(name, age, gender)
+        this.course = course
+        this.university = university
+    }
+    study() {
+        return `${this.name}, Ви навчаєтеся на ${this.course} курсі в ${this.university}`
+    }
+}
+
+class Teacher extends Person{
+    constructor(name, age, gender, subject, university){
+        super(name, age, gender)
+        this.subject = subject
+        this.university = university
+    }
+    teach(){
+        return `${this.name}, Ви маєте предмет ${this.subject} в ${this.university}`
+    }
+}
+
+const newStudent = new Student('Kate', 19, 'female', 2, 'Lviv University')
+// console.log(newStudent.greet());
+// console.log(newStudent.study());
+
+const newTeacher = new Teacher('Mike', 43, 'male', 'history', 'Lviv University')
+// console.log(newTeacher.teach());
+
+
+// 12. Створіть клас BankAccount, який містить наступні властивості:
+// • balance (початковий баланс рахунку)
+// • transactions (масив транзакцій)
+// Також створіть наступні методи:
+// • deposit(amount) - додає суму до балансу рахунку та додає нову транзакцію з описом "депозит" та зазначенням суми
+// • withdraw(amount) - знімає суму з балансу рахунку, якщо на балансі достатньо коштів, та додає нову транзакцію з описом "виведення" та зазначенням суми
+// • getBalance() - повертає поточний баланс рахунку
+// • getTransactionHistory() - повертає масив з історією транзакцій (тобто всі транзакції, які були здійснені на цьому рахунку), де кожна транзакція містить опис та суму
+
+class BankAccount {
+    constructor(balance, transactions){
+        this.balance = balance
+        this.transactions = []
+    }
+    deposit(amount){
+        this.transactions.push(`На рахунок покладено ${amount} грн`)
+        return this.balance += amount
+    }
+    withdraw(amount){
+        if (this.balance - amount >= 0) {
+            this.transactions.push(`З рахунку виведено ${amount} грн`)
+            return this.balance -= amount
+        } else {
+            this.transactions.push(`Відмова у виведенні ${amount} грн з рахунку. Недостатньо коштів на рахунку`)
+            return this.balance
+        }
+    }
+    getBalance(){
+        return this.balance
+    }
+    getTransactionHistory(){
+        return this.transactions
+    }
+}
+
+const newBank = new BankAccount(25000)
+// console.log(newBank.deposit(400));
+// console.log(newBank.withdraw(700));
+// console.log(newBank.withdraw(30000));
+// console.log(newBank.getTransactionHistory());
+// console.log(newBank.getBalance());
+
+
+// 13. Створіть клас Person, який містить наступні властивості:
+// • firstName (ім'я)
+// • lastName (прізвище)
+// • age (вік)
+// • gender (стать)
+// • interests (масив інтересів)
+// Також створіть метод bio(), який повертає рядок з інформацією про особу, наприклад "Мене звати Іван Петров, мені 25 років. Я люблю грати в футбол та грати на гітарі."
+
+class Person1 {
+    constructor(firstName, lastName, age, gender, interests){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.gender = gender
+        this.interests = interests
+    }
+    bio(){
+        return `Мене звати ${this.firstName} ${this.lastName}, мені ${this.age} років. Я люблю ${this.interests.join(', ')}.`
+    }
+}
+
+const newPerson1 = new Person1 ('Іван', 'Петров', 25, 'male', ['грати в футбол', 'грати на гітарі'])
+console.log(newPerson1.bio());
+
+
+// 14. Створіть клас Person з властивостями firstName, lastName, gender, age та методом getFullName(), який повертає повне ім'я людини у форматі "Прізвище Ім'я". 
+// Створіть підклас Employee з властивостями jobTitle та salary. Додайте до класу Employee метод getAnnualSalary(), який повертає річний дохід працівника (за умови, що він працює цілий рік). Створіть екземпляр класу Employee та викличте методи getFullName() та getAnnualSalary() з цього екземпляру
+
+class Person2 {
+    constructor(firstName, lastName, gender, age){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.gender = gender
+        this.age = age
+    }
+    getFullName(){
+        return `${this.lastName} ${this.firstName}`
+}
+}
+
+class Employee extends Person2{
+    constructor(firstName, lastName, gender, age, jobTitle, salary){
+        super(firstName, lastName, gender, age)
+        this.jobTitle = jobTitle
+        this.salary = salary
+    }
+    getAnnualSalary(){
+        return `Ваш річний дохід ${this.salary * 12} грн`
+    }
+}
+
+const newEmployee = new Employee ('Іван', 'Петров', 'male', 25, 'менеджер', 15000)
+console.log(newEmployee.getFullName());
+console.log(newEmployee.getAnnualSalary());
+
+
+// 15. Створіть клас Cart, який має властивість items, яка містить список продуктів (екземплярів класу Product) та методи addItem(product, quantity), removeItem(product),  getTotalPrice() та clear(). Метод addItem додає продукт у кошик з певною кількістю, метод removeItem видаляє продукт з кошика, метод getTotalPrice повертає загальну вартість всіх  продуктів у кошику, а метод clear очищає кошик.
+
+class Cart {
+    constructor(){
+        this.items = items
+    }
+    addItem(product, quantity){
+
+    }
+    removeItem(product){
+
+    }
+    getTotalPrice(){
+
+    }
+    clear(){
+        
+    }
+}
